0625238a72bd771642a59a4de33a8932fe8728b0
Yozhikvtumane <yoshkin@kot.ks>
2025-05-13 23:59:03 +0200

    Initial frontend commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    // Правильный путь через прокси
+    fetch("https://mern-backend-wzrm.onrender.com/api/test")
+      .then((res) => {
+        if (!res.ok) throw new Error(res.statusText);
+        return res.json();
+      })
+      .then((data) => setData(data.message))
+      .catch((err) => {
+        console.error("Ошибка:", err);
+        setData("Ошибка подключения к бэкенду");
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>MERN App</h1>
+      <p>Ответ от бэкенда: {data}</p>
     </div>
   );
 }
