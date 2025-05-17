bc045eaf44ce2382351b9759d5f8fae891594a11
Yozhikvtumane <yoshkin@kot.ks>
2025-05-17 08:44:34 +0200

    ... some hero styles updated ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header/Header";
 import { useEffect, useState } from "react";
 import Hero from "./components/Hero/Hero";
+import Features from "./components/Features/Features";
 
 function App() {
   const [data, setData] = useState("");
@@ -22,6 +23,7 @@ function App() {
     <div>
       <Header></Header>
       <Hero></Hero>
+      <Features></Features>
       <p>Ответ от бэкенда: {data}</p>
     </div>
   );
