b1e6ca4d39c58e4edba0b4f1ea32eb2e981d3b21
Yozhikvtumane <yoshkin@kot.ks>
2025-05-17 10:40:58 +0200

    dummy footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,17 @@
 import Header from "./components/Header/Header";
-import { useEffect, useState } from "react";
 import Hero from "./components/Hero/Hero";
 import Features from "./components/Features/Features";
 import RecipesCatalog from "./components/RecipesCatalog/RecipesCatalog";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  const [data, setData] = useState("");
-
-  useEffect(() => {
-    fetch("https://mern-backend-wzrm.onrender.com/api/test")
-      .then((res) => {
-        if (!res.ok) throw new Error(res.statusText);
-        return res.json();
-      })
-      .then((data) => setData(data.message))
-      .catch((err) => {
-        console.error("Ошибка:", err);
-        setData("Ошибка подключения к бэкенду");
-      });
-  }, []);
-
   return (
     <div>
       <Header></Header>
       <Hero></Hero>
       <Features></Features>
       <RecipesCatalog></RecipesCatalog>
-      <p>Ответ от бэкенда: {data}</p>
+      <Footer></Footer>
     </div>
   );
 }
