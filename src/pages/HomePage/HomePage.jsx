bbc6769fe654b51771cb561a4ab874c0f4101045
Yozhikvtumane <yoshkin@kot.ks>
2025-05-18 21:57:48 +0200

    upload form styles

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import Hero from "../../components/HomePage/Hero/Hero";
 import Features from "../../components/HomePage/Features/Features";
 import RecipesCatalog from "../../components/HomePage/RecipesCatalog/RecipesCatalog";
-import ImageUploadForm from "../../components/ImageUploadForm";
+import ImageUploadForm from "../../components/ImageUploadForm/ImageUploadForm";
 
 export default function HomePage() {
   return (
