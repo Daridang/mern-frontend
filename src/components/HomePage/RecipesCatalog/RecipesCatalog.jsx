fa46c9a5c90b67608bf175ac0a1bfefa1764ae75
Yozhikvtumane <yoshkin@kot.ks>
2025-05-18 10:44:48 +0200

    recipe catalog updated with new items, helper function created

--- src/components/HomePage/RecipesCatalog/RecipesCatalog.jsx
+++ src/components/HomePage/RecipesCatalog/RecipesCatalog.jsx
@@ -1,26 +1,18 @@
 // src/components/RecipesCatalog/RecipesCatalog.jsx
 import React from "react";
 import styles from "./RecipesCatalog.module.css";
-
-import galacticSushi from "../../../assets/galactic_sushi.webp";
-import neonNoodles from "../../../assets/neon_noodles.webp";
-import astroBurger from "../../../assets/astro_burger.webp";
-import cryoDesert from "../../../assets/cryo_desert.webp";
-import cryoDesertPink from "../../../assets/cryo_desert_pink.webp";
-import printedLive3D from "../../../assets/3d_printed_live.webp";
-
 import RecipeCard from "../RecipeCard/RecipeCard";
+import sampleData from "../../../assets/data/recipes.json";
 
-const sampleRecipes = [
-  { id: 1, title: "Galactic Sushi", price: "$14.99", img: galacticSushi },
-  { id: 2, title: "Neon Noodles", price: "$11.50", img: neonNoodles },
-  { id: 3, title: "Astro Burger", price: "$13.25", img: astroBurger },
-  { id: 4, title: "Cryo Desert", price: "$9.75", img: cryoDesert },
-  { id: 5, title: "Cryo Desert Pink", price: "$10.25", img: cryoDesertPink },
-  { id: 6, title: "3D Printed Live", price: "$16.00", img: printedLive3D },
-  { id: 7, title: "Cryo Desert Pink", price: "$10.25", img: cryoDesertPink },
-  { id: 8, title: "3D Printed Live", price: "$16.00", img: printedLive3D },
-];
+function transformRecipesToCatalog(data) {
+  return data.map((recipe, index) => ({
+    id: recipe.id ?? index + 1,
+    title: recipe.title,
+    price: `$${(Math.random() * 10 + 8).toFixed(2)}`,
+    img: recipe.image,
+  }));
+}
+const sampleRecipes = transformRecipesToCatalog(sampleData);
 
 export default function RecipesCatalog({ recipes = sampleRecipes }) {
   return (
